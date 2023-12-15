526c1277357776158f88344723d93af811ebd0db
keisukemurota <mukeisuke0709@gmail.com>
2023-12-15 01:20:31 +0100

    added todo comment

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -47,13 +47,17 @@ return {
 			local dap = require("dap")
 
 			local command = vim.fn.stdpath("data") .. "/mason/bin/OpenDebugAD7"
-			dap.adapters.codelldb = {
-				args = {"-"},
-			}
+			-- dap.adapters.cppdbg2 = {
+			-- 	id = "cppdbg",
+			-- 	type = "executable",
+			-- 	command = command,
+			-- 	MIMode = "lldb",
+			-- 	miDebuggerPath = "~/.vscode/extensions/ms-vscode.cpptools-1.19.1-darwin-arm64/debugAdapters/lldb-mi/bin/lldb-mi",
+			-- }
 
 			dap.configurations.cpp = {
 				{
-					name = "Debug with gdb",
+					name = "Debug with codelldb",
 					type = "codelldb",
 					request = "launch",
 					program = function()
@@ -65,6 +69,8 @@ return {
 					end,
 					cwd = "/Users/keisukemurota/Documents/todo/worms/Debug",
 					stopOnEntry = false,
+					showDisassembly = "never",
+					args = {},
 				},
 			}
 		end,
@@ -532,6 +538,9 @@ return {
 			"CMakeRun",
 			"CMakeOpen",
 			"CMakeGenerate",
+			"CMakeDebug",
+			"CMakeStop",
+			"CMakeQuickDebug",
 		},
 		config = function()
 			require("cmake-tools").setup({
@@ -550,7 +559,7 @@ return {
 				},
 				cmake_dap_configuration = {                                   -- debug settings for cmake
 					name = "cpp",
-					type = "codelldb",
+					type = "codelldb",                                          -- or you can use 'cpptools' / 'codelldb'
 					request = "launch",
 					stopOnEntry = false,
 					runInTerminal = true,
@@ -599,5 +608,78 @@ return {
 				},
 			})
 		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		lazy = false,
+		opts = {
+
+			signs = true,   -- show icons in the signs column
+			sign_priority = 8, -- sign priority
+			-- keywords recognized as todo comments
+			keywords = {
+				FIX = {
+					icon = " ", -- icon used for the sign, and in search results
+					color = "error", -- can be a hex color, or a named color (see below)
+					alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "f" }, -- a set of other keywords that all map to this FIX keywords
+					-- signs = false, -- configure signs for some keywords individually
+				},
+				TODO = { icon = " ", color = "info" },
+				HACK = { icon = " ", color = "warning" },
+				WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+				PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+				NOTE = { icon = " ", color = "hint", alt = { "n" } },
+				INFO = { icon = " ", color = "comm", alt = { "i" } },
+				TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+				-- Iconf for fix (spana) :                                                         
+			},
+			gui_style = {
+				fg = "NONE",      -- The gui style to use for the fg highlight group.
+				bg = "BOLD",      -- The gui style to use for the bg highlight group.
+			},
+			merge_keywords = true, -- when true, custom keywords will be merged with the defaults
+			-- highlighting of the line containing the todo comment
+			-- * before: highlights before the keyword (typically comment characters)
+			-- * keyword: highlights of the keyword
+			-- * after: highlights after the keyword (todo text)
+			highlight = {
+				multiline = true,               -- enable multine todo comments
+				multiline_pattern = "^.",       -- lua pattern to match the next multiline from the start of the matched keyword
+				multiline_context = 10,         -- extra lines that will be re-evaluated when changing a line
+				before = "fg",                  -- "fg" or "bg" or empty
+				keyword = "fg",                 -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+				after = "fg",                   -- "fg" or "bg" or empty
+				pattern = { [[\#\s*<(KEYWORDS)\s*:]], [[//\s*<(KEYWORDS)\s*:]], [[--\s*<(KEYWORDS)\s*:]], [[%\s*<(KEYWORDS)\s*:]] }, -- pattern or table of patterns, used for highlighting (vim regex) (I modifed so that it triggers when keyword appear right after #)
+				comments_only = true,           -- uses treesitter to match keywords in comments only
+				max_line_len = 400,             -- ignore lines longer than this
+				exclude = {},                   -- list of file types to exclude highlighting
+			},
+			-- list of named colors where we try to extract the guifg from the
+			-- list of highlight groups or use the hex color if hl not found as a fallback
+			colors = {
+				error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+				warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+				info = { "DiagnosticInfo", "#2563EB" },
+				hint = { "#10B981" },
+				comm = { "#34eb5e" },
+				default = { "Identifier", "#7C3AED" },
+				test = { "Identifier", "#FF00FF" },
+			},
+			search = {
+				command = "rg",
+				args = {
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+				},
+				-- regex that will be used to match keywords.
+				-- don't replace the (KEYWORDS) placeholder
+				pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+				-- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+			},
+		},
 	},
 }

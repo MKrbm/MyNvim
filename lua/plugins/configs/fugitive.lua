90df28012cba478f374d7094c38d3c84daf09364
MKrbm <mukeisuke0709@gmail.com>
2024-06-09 01:14:30 +0900

    updated keymap

--- lua/plugins/configs/fugitive.lua
+++ lua/plugins/configs/fugitive.lua
@@ -29,5 +29,5 @@ autocmd("BufWinEnter", {
 })
 
 
-vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
-vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+-- vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
+-- vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")

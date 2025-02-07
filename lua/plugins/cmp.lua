c2af598614ba9cbbde4f77e7f9ebaea54236866a
MKrbm <mukeisuke0709@gmail.com>
2025-02-07 17:39:49 +0900

    updated lua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,76 +1,83 @@
 return {
-    {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
-        dependencies = {
-            {
-                -- snippet plugin
-                "L3MON4D3/LuaSnip",
-                -- dependencies = "rafamadriz/friendly-snippets",
-                opts = {
-                    history = true,
-                    updateevents = "TextChanged,TextChangedI"
-                },
-                config = function(_, opts)
-                    require("luasnip").config.set_config(opts)
-                    -- snipmate format
-                    require("luasnip.loaders.from_snipmate").load()
-                    require("luasnip.loaders.from_snipmate").lazy_load({
-                        paths = vim.g.snipmate_snippets_path or ""
-                    })
-                    -- lua format
-                    require("luasnip.loaders.from_lua").load()
-                    require("luasnip.loaders.from_lua").lazy_load({
-                        paths = vim.g.lua_snippets_path or ""
-                    })
-                    vim.api.nvim_create_autocmd("InsertLeave", {
-                        callback = function()
-                            if require("luasnip").session.current_nodes[vim.api
-                                .nvim_get_current_buf()] and
-                                not require("luasnip").session.jump_active then
-                                require("luasnip").unlink_current()
-                            end
-                        end
-                    })
-                end
-            }, {
-                "windwp/nvim-autopairs",
-                opts = {
-                    fast_wrap = {},
-                    disable_filetype = {"TelescopePrompt", "vim"}
-                }
-            }, -- cmp sources plugins
-            {
-                "saadparwaiz1/cmp_luasnip", "hrsh7th/cmp-nvim-lua",
-                "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path"
-                -- "hrsh7th/cmp-cmdline",
-            }
-        },
-        opts = function() return require("plugins.configs.cmp") end,
-        config = function(_, opts)
-            cmp = require("cmp")
-            cmp.setup(opts)
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			{
+				-- snippet plugin
+				"L3MON4D3/LuaSnip",
+				-- dependencies = "rafamadriz/friendly-snippets",
+				opts = {
+					history = true,
+					updateevents = "TextChanged,TextChangedI",
+				},
+				config = function(_, opts)
+					require("luasnip").config.set_config(opts)
+					-- snipmate format
+					require("luasnip.loaders.from_snipmate").load()
+					require("luasnip.loaders.from_snipmate").lazy_load({
+						paths = vim.g.snipmate_snippets_path or "",
+					})
+					-- lua format
+					require("luasnip.loaders.from_lua").load()
+					require("luasnip.loaders.from_lua").lazy_load({
+						paths = vim.g.lua_snippets_path or "",
+					})
+					vim.api.nvim_create_autocmd("InsertLeave", {
+						callback = function()
+							if
+									require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+									and not require("luasnip").session.jump_active
+							then
+								require("luasnip").unlink_current()
+							end
+						end,
+					})
+				end,
+			},
+			{
+				"windwp/nvim-autopairs",
+				opts = {
+					fast_wrap = {},
+					disable_filetype = { "TelescopePrompt", "vim" },
+				},
+			}, -- cmp sources plugins
+			{
+				"saadparwaiz1/cmp_luasnip",
+				"hrsh7th/cmp-nvim-lua",
+				"hrsh7th/cmp-nvim-lsp",
+				"hrsh7th/cmp-buffer",
+				"hrsh7th/cmp-path",
+				-- "hrsh7th/cmp-cmdline",
+			},
+		},
+		opts = function()
+			return require("plugins.configs.cmp")
+		end,
+		config = function(_, opts)
+			cmp = require("cmp")
+			cmp.setup(opts)
 
-            -- cmp.setup.cmdline("/", {
-            -- 	mapping = cmp.mapping.preset.cmdline(),
-            -- 	sources = {
-            -- 		{ name = "buffer" },
-            -- 	},
-            -- })
+			-- cmp.setup.cmdline("/", {
+			-- 	mapping = cmp.mapping.preset.cmdline(),
+			-- 	sources = {
+			-- 		{ name = "buffer" },
+			-- 	},
+			-- })
 
-            -- cmp.setup.cmdline(":", {
-            -- 	mapping = cmp.mapping.preset.cmdline(),
-            -- 	sources = cmp.config.sources({
-            -- 		{ name = "path" },
-            -- 	}, {
-            -- 		{
-            -- 			name = "cmdline",
-            -- 			option = {
-            -- 				ignore_cmds = { "Man", "!" },
-            -- 			},
-            -- 		},
-            -- 	}),
-            -- })
-        end
-    }
+			-- cmp.setup.cmdline(":", {
+			-- 	mapping = cmp.mapping.preset.cmdline(),
+			-- 	sources = cmp.config.sources({
+			-- 		{ name = "path" },
+			-- 	}, {
+			-- 		{
+			-- 			name = "cmdline",
+			-- 			option = {
+			-- 				ignore_cmds = { "Man", "!" },
+			-- 			},
+			-- 		},
+			-- 	}),
+			-- })
+		end,
+	},
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -11,24 +11,4 @@ return {
       -- opts = function() return require("plugins.configs.ibl") end,
       config = function() return require("plugins.configs.ibl") end
   }, -- DAP (Debug Adapter Protocol)
-	{
-		"iamcco/markdown-preview.nvim",
-		ft = { "markdown" },
-		build = ":call mkdp#util#install()",
-	},
-	{
-		"chentoast/marks.nvim",
-		lazy = false,
-		config = function()
-			require("marks").setup({
-				mappings = {
-					set_next = "m,",
-					next = "m]",
-					preview = "m:",
-					set_bookmark0 = "m0",
-					prev = false, -- pass false to disable only this default mapping
-				},
-			})
-		end,
-	},
 }
